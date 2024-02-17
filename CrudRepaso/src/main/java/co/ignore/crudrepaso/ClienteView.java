1eff11b8914487717193f3d84a6a8208299ef386
Andres-Correa69 <correavalencia334@gmail.com>
2024-02-17 01:50:18 -0500

    solucion error controlador

--- CrudRepaso/src/main/java/co/ignore/crudrepaso/ClienteView.java
+++ CrudRepaso/src/main/java/co/ignore/crudrepaso/ClienteView.java
@@ -1,9 +1,127 @@
 package co.ignore.crudrepaso;
 
 import javafx.fxml.FXML;
+import javafx.scene.control.Button;
 import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
 
 public class ClienteView {
 
 
+    @FXML
+    private Button addCliente;
+
+    @FXML
+    private Button addEnva;
+
+    @FXML
+    private Button addPere;
+
+    @FXML
+    private Button addRefri;
+
+    @FXML
+    private Button deleteCliente;
+
+    @FXML
+    private Button deleteEnva;
+
+    @FXML
+    private Button deletePere;
+
+    @FXML
+    private Button deleteRefri;
+
+    @FXML
+    private Button editCliente;
+
+    @FXML
+    private Button editEnva;
+
+    @FXML
+    private Button editPere;
+
+    @FXML
+    private Button editRefri;
+
+    @FXML
+    private TextField inApellido;
+
+    @FXML
+    private TextField inCantEnva;
+
+    @FXML
+    private TextField inCantPere;
+
+    @FXML
+    private TextField inCantRefri;
+
+    @FXML
+    private TextField inCodAproRefri;
+
+    @FXML
+    private TextField inCodEnva;
+
+    @FXML
+    private TextField inCodPere;
+
+    @FXML
+    private TextField inCodRefri;
+
+    @FXML
+    private TextField inDesEnva;
+
+    @FXML
+    private TextField inDesPere;
+
+    @FXML
+    private TextField inDesRefri;
+
+    @FXML
+    private TextField inDireccion;
+
+    @FXML
+    private TextField inDocumento;
+
+    @FXML
+    private TextField inEmail;
+
+    @FXML
+    private TextField inFechaEnva;
+
+    @FXML
+    private TextField inFechaNacimiento;
+
+    @FXML
+    private TextField inFechaVenPere;
+
+    @FXML
+    private TextField inNomEnva;
+
+    @FXML
+    private TextField inNomPere;
+
+    @FXML
+    private TextField inNomRefri;
+
+    @FXML
+    private TextField inNombre;
+
+    @FXML
+    private TextField inPesoEnva;
+
+    @FXML
+    private TextField inTelefono;
+
+    @FXML
+    private TextField inTempRefri;
+
+    @FXML
+    private TextField inValEnva;
+
+    @FXML
+    private TextField inValPere;
+
+    @FXML
+    private TextField inValRefri;
 }
--- CrudRepaso/src/main/java/co/ignore/crudrepaso/HelloApplication.java
+++ CrudRepaso/src/main/java/co/ignore/crudrepaso/HelloApplication.java
@@ -12,6 +12,7 @@ public class HelloApplication extends Application {
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("ClienteView.fxml"));
         Scene scene = new Scene(fxmlLoader.load(), 1233, 703);
+
         stage.setTitle("Crud");
         stage.setScene(scene);
         stage.show();
