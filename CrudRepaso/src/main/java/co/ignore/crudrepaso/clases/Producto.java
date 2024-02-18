91a0dcea1ef0bb9908f8395fbfeec1dcb6f8c9b8
Marcela0399 <142555238+Marcela0399@users.noreply.github.com>
2024-02-17 20:27:15 -0500

    producto

--- CrudRepaso/src/main/java/co/ignore/crudrepaso/clases/Producto.java
+++ CrudRepaso/src/main/java/co/ignore/crudrepaso/clases/Producto.java
@@ -9,14 +9,30 @@ public class Producto {
     private String descripcion;
     private Integer valorUnitario;
     private Integer cantExistente;
+    private LocalDate fechaEnvasado;
+    private Integer peso;
+    private PaisOrigen pais;
+    private LocalDate fechaVencimiento;
+    private String codigoAprovacion;
+    private String temperaturaRefrigeracion;
 
     //metodo constructor
-    public Producto(String codigo, String nombre, String descripcion, Integer valorUnitario, Integer cantExistente) {
+    public Producto(String codigo, String nombre, String descripcion, Integer valorUnitario, Integer cantExistente,
+                    LocalDate fechaEnvasado, Integer peso, PaisOrigen pais, LocalDate fechaVencimiento, String codigoAprovacion,
+                    String temperaturaRefrigeracion) {
         this.codigo = codigo;
         this.nombre = nombre;
         this.descripcion = descripcion;
         this.valorUnitario = valorUnitario;
         this.cantExistente = cantExistente;
+        this.fechaEnvasado = fechaEnvasado;
+        this.peso = peso;
+        this.pais = pais.COLOMBIA;
+        this.fechaVencimiento = fechaVencimiento;
+        this.codigoAprovacion = codigoAprovacion;
+        this.temperaturaRefrigeracion = temperaturaRefrigeracion;
+
+
     }
 
     //getter y setters
@@ -59,5 +75,53 @@ public class Producto {
 
     public void setCantExistente(Integer cantExistente) {
         this.cantExistente = cantExistente;
+    }
+
+    public LocalDate getFechaEnvasado() {
+        return fechaEnvasado;
+    }
+
+    public void setFechaEnvasado(LocalDate fechaEnvasado) {
+        this.fechaEnvasado = fechaEnvasado;
+    }
+
+    public Integer getPeso() {
+        return peso;
+    }
+
+    public void setPeso(Integer peso) {
+        this.peso = peso;
+    }
+
+    public PaisOrigen getPais() {
+        return pais;
+    }
+
+    public void setPais(PaisOrigen pais) {
+        this.pais = pais;
+    }
+
+    public LocalDate getFechaVencimiento() {
+        return fechaVencimiento;
+    }
+
+    public void setFechaVencimiento(LocalDate fechaVencimiento) {
+        this.fechaVencimiento = fechaVencimiento;
+    }
+    
+    public String getCodigoAprovacion() {
+        return codigoAprovacion;
+    }
+
+    public void setCodigoAprovacion(String codigoAprovacion) {
+        this.codigoAprovacion = codigoAprovacion;
+    }
+
+    public String getTemperaturaRefrigeracion() {
+        return temperaturaRefrigeracion;
+    }
+
+    public void setTemperaturaRefrigeracion(String temperaturaRefrigeracion) {
+        this.temperaturaRefrigeracion = temperaturaRefrigeracion;
     }
 }
